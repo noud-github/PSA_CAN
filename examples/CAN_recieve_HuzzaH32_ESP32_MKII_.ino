e82b58809ce3947ed42c93edce285a1c89db5787
Arnoud Thoerig <arnoud@thorig.eu>
2021-08-20 21:24:04 +0200

    add examples

---
+++ examples/CAN_recieve_HuzzaH32_ESP32_MKII_.ino
@@ -0,0 +1,119 @@
+#include <ESP32CAN.h>
+#include <CAN_config.h>
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adapted_SSD1306.h>
+
+#define OLED_RESET 4
+Adafruit_SSD1306 display(OLED_RESET);
+//Analog Input
+#define ANALOG_PIN_0 A4
+int analog_value = 0;
+
+// PMW SERVO  //lange zijde breadboard nummers PLUS #2 Ground #4 data #13 
+#define SERVO_PIN 19
+int freq = 50;
+int channel = 0;
+int resolution = 8;
+int dutyCycle = 21;
+
+//Can bus
+CAN_device_t CAN_cfg;
+
+char msgString[128];                        // Array to store serial string
+bool DataChanged = true;
+int hours = 0;
+int minutes = 0;
+int temperature = 21;
+
+#if (SSD1306_LCDHEIGHT != 32)
+#error("Height incorrect, please fix Adafruit_SSD1306.h!");
+#endif
+
+void setup() {
+    Serial.begin(115200);
+    Serial.println("iotsharing.com CAN demo");
+    CAN_cfg.speed=CAN_SPEED_125KBPS;
+    CAN_cfg.tx_pin_id = GPIO_NUM_5;
+    CAN_cfg.rx_pin_id = GPIO_NUM_4;
+    CAN_cfg.rx_queue = xQueueCreate(10,sizeof(CAN_frame_t));
+    //start CAN Module
+    ESP32Can.CANInit();
+    display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // initialize with the I2C addr 0x3C (for the 128x32)
+    display.display();
+    delay(2000);
+    // Clear the buffer.
+    display.clearDisplay();
+    display.setTextSize(1);
+    display.setTextColor(WHITE);
+    display.setCursor(0,0);
+    display.print("00:00");
+    display.setCursor(110,0);
+    display.print("-00");
+    display.display();
+
+    Serial.println("ESP32 Servo Control");
+
+    ledcSetup(channel, freq, resolution);
+    ledcAttachPin(SERVO_PIN, channel);
+    ledcWrite(channel, dutyCycle);
+
+
+
+}
+
+void loop() {
+  
+    
+    analog_value = analogRead(ANALOG_PIN_0);
+  //Serial.print(analog_value);
+  //Serial.print(" Duty Cycle ==> ");
+  //Serial.println(dutyCycle);
+  dutyCycle = map(analog_value, 0, 4095, 28, 7);
+  ledcWrite(channel, dutyCycle);
+  delay(50);
+    
+    CAN_frame_t rx_frame;
+    //receive next CAN frame from queue
+    if(xQueueReceive(CAN_cfg.rx_queue,&rx_frame, 3*portTICK_PERIOD_MS)==pdTRUE){
+      //do stuff!
+      if(rx_frame.FIR.B.FF==CAN_frame_std)
+        //Serial.println(rx_frame.MsgID);
+        if (rx_frame.MsgID == 0x276){
+          //hours
+          hours = (rx_frame.data.u8[3] & 0b00011111) ;
+         //min
+          minutes = (rx_frame.data.u8[4] & 0b00111111) ;
+          DataChanged = true;
+        }
+        
+        //Serial.println(DataChanged);
+        if (DataChanged) {
+          display.clearDisplay();
+          display.setTextSize(1);
+          display.setTextColor(WHITE);
+        if (hours<10){
+          display.setCursor(1,0);
+        }else{
+          display.setCursor(0,0);
+        }
+        display.print(hours);
+        display.print(":");
+        if (minutes<10) display.print('0');
+        display.print(minutes);
+        display.setCursor(110,0);
+        if (temperature < 0 | temperature >= 100){
+          display.setCursor(110,0);
+        }else{
+          display.setCursor(111,0);
+        }
+        display.print(temperature);
+
+      
+    }
+    //Serial.println()
+    display.display();
+    DataChanged = false;
+    }
+}
---
+++ examples/CAN_send_MCP_can_MCP2515_mkII.ino
@@ -0,0 +1,225 @@
+// CAN Send Example
+//
+
+#include <mcp_can.h>
+#include <SPI.h>
+#include <PSA_CAN.h>
+
+/*
+typedef struct {
+  byte LeftFront;
+  byte CenterFront;
+  byte RightFront;
+  byte LeftRear;
+  byte CenterRear;
+  byte RightRear;
+  bool Show;
+} PDC;
+*/
+
+MCP_CAN CAN0(10);     // Set CS to pin 10
+msgId_0E1 parktronic;
+
+void setup()
+{
+  Serial.begin(115200);
+  pinMode(3,INPUT_PULLUP);
+  pinMode(4, INPUT_PULLUP);
+  //digitalWrite(3, HIGH); //turn pullup resistor on
+  //digitalWrite(4, HIGH); //turn pullup resistor on
+
+  // Initialize MCP2515 running at 16MHz with a baudrate of 125kb/s and the masks and filters disabled.
+  START_INIT:
+    // Initialize MCP2515 running at 16MHz with a baudrate of 125kb/s and the masks and filters disabled.
+    if(CAN0.begin(MCP_ANY, CAN_125KBPS, MCP_16MHZ) == CAN_OK)                   // init can bus : baudrate = 500k
+    {
+        Serial.println("MCP2515 Initialized Successfully!");
+    }
+    else
+    {
+        Serial.println("Error Initializing MCP2515...");
+        Serial.println("Init CAN BUS Shield again");
+        delay(100);
+        goto START_INIT;
+    }
+
+  CAN0.setMode(MCP_NORMAL);   // Change to normal mode to allow messages to be transmitted
+}
+
+void loop()
+{
+  Check_if_Button_Pressed();
+  CAN_Sent_0E1_maintain();
+  if (parktronic.getShowWindowValue()){ 
+    PDC_Action_maintain();
+  }
+
+ //Serial.println (parktronic.Period);
+}
+
+void CAN_Sent_0E1_maintain()
+{
+  
+  //static const unsigned long REFRESH_INTERVAL = 100; // ms
+  static unsigned long lastRefreshTime = 0;
+  if(millis() - lastRefreshTime >= parktronic.Period) 
+  {
+    lastRefreshTime += parktronic.Period;
+                CAN_Sent_0E1();
+  } else { 
+    if (millis() - lastRefreshTime < 0){
+          lastRefreshTime = 0;
+          CAN_Sent_0E1();
+    }
+  }
+}
+
+void CAN_Sent_0E1(){
+   //0E1
+   //Sent on: period 100
+   unsigned char txBuf_0E1[7] = {0b00000000,0b00000000,0b00101010,0b00000000,0b00000000,0b00000000,0b11111111};
+   static byte Sendframe = 2;
+    
+   if (parktronic.getShowWindowValue()){
+      parktronic.getMsgBuf(txBuf_0E1); // get Msg buffer
+      Sendframe = 2;
+     
+   }    
+   if (Sendframe > 0){
+   
+    byte sndStat = CAN0.sendMsgBuf(0x0E1, 0, 7, txBuf_0E1);
+    if(sndStat == CAN_OK){
+      //Serial.println("Message Sent Successfully!");
+    } else {
+      Serial.println("Error Sending Message...");
+    }
+    Sendframe-- ;
+   }
+   
+}
+
+
+void Check_if_Button_Pressed(){
+  static int  switchState_A= LOW;
+  static int  switchReading_A=LOW;
+  static int prevswitchState_A= LOW;
+  static int  switchReading_B=LOW;
+  static int  switchState_B=LOW;
+  static int prevswitchState_B= LOW;
+  static unsigned long lastDebounceTime_A = 0;  // the last time the output pin was toggled
+  static unsigned long lastDebounceTime_B = 0;  // the last time the output pin was toggled
+  static unsigned long debounceDelay = 50;    // the debounce time; increase if the output flickers
+
+  
+  switchReading_A = digitalRead(3);
+  if (switchReading_A != prevswitchState_A){
+      //Serial.println("Pressed_A");
+      lastDebounceTime_A = millis();
+      //parktronic.setShowWindowValue ( !((bool)parktronic.getShowWindowValue()));
+     
+  }
+
+  if ((millis() - lastDebounceTime_A) > debounceDelay) {
+    // whatever the reading is at, it's been there for longer than the debounce
+    // delay, so take it as the actual current state:
+
+    // if the button state has changed:
+    if (switchReading_A != switchState_A) {
+      switchState_A = switchReading_A;
+     
+        
+      // only toggle the LED if the new button state is HIGH
+      if (switchState_A  == LOW) {
+         Serial.println("Pressed_A");
+        parktronic.setShowWindowValue ( !((bool)parktronic.getShowWindowValue()));
+      }
+    }
+  }
+  prevswitchState_A = switchReading_A;
+
+  switchReading_B = digitalRead(4);
+  if (switchReading_B != prevswitchState_B){
+      //Serial.println("Pressed_B");
+      lastDebounceTime_B = millis();
+     
+     
+  }
+
+  if ((millis() - lastDebounceTime_B) > debounceDelay) {
+    // whatever the reading is at, it's been there for longer than the debounce
+    // delay, so take it as the actual current state:
+
+    // if the button state has changed:
+    if (switchReading_B != switchState_B) {
+      switchState_B = switchReading_B;
+      
+        
+      // only toggle the LED if the new button state is HIGH
+      if (switchState_B  == LOW) {
+        Serial.println("Pressed_B");  
+      }
+    }
+  }
+  prevswitchState_B = switchReading_B;
+  
+}
+
+void PDC_Action_maintain()
+{
+  
+  static const unsigned long REFRESH_INTERVAL = 750; // ms
+  static unsigned long lastRefreshTime = 0;
+  if(millis() - lastRefreshTime >= REFRESH_INTERVAL) 
+  {
+    lastRefreshTime += REFRESH_INTERVAL;
+                PDC_Action();
+  } else { 
+    if (millis() - lastRefreshTime < 0){
+          lastRefreshTime = 0;
+          PDC_Action();
+    }
+  }
+}
+
+void PDC_Action(){
+  
+ static int i = -7;
+ static int j = -7;
+ static int k = -7;
+ static bool front = false;
+ if (front){
+    parktronic.setRearCenterBits(abs(7));
+    parktronic.setRearLeftBits(abs(7));
+    parktronic.setRearRightBits(abs(7));
+    parktronic.setFrontCenterBits(abs(i));
+    parktronic.setFrontLeftBits(abs(j));
+    parktronic.setFrontRightBits(abs(k));
+ }else{
+    parktronic.setRearCenterBits(abs(i));
+    parktronic.setRearLeftBits(abs(j));
+    parktronic.setRearRightBits(abs(k));
+    parktronic.setFrontCenterBits(abs(7));
+    parktronic.setFrontLeftBits(abs(7));
+    parktronic.setFrontRightBits(abs(7));
+ }
+ i = i + random(-3,3);
+ j = j + random(-3,3);
+ k = k + random(-3,3);
+
+ if (abs(k)+abs(j)+abs(i) > 18)
+   front = !front;
+ 
+ if (abs(i) > 7){
+  i = 7 ;
+ }
+ if (abs(j) > 7){
+  j = 7 ;
+ }
+ if (abs(k) > 7){
+  k = 7 ;
+ }
+}
+
+/*********************************************************************************************************
+  END FILE
+*********************************************************************************************************/
